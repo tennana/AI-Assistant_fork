7a8694d5bca229e22d128675384fa06cc0ee5ca3
Tennana <tennana@tennana.net>
2024-06-09 01:11:15 +0900

    fixup! feat: クリップボードコピー

--- AI_Assistant_modules/output_image_gui.py
+++ AI_Assistant_modules/output_image_gui.py
@@ -18,19 +18,24 @@ function copyToClipboard() {
 
 class OutputImage:
     def __init__(self, transfer_target=None):
+        self.output_image_path = None
         self.transfer_target = transfer_target
 
     def layout(self, lang_util):
-        self.output_image = gr.Image(label=lang_util.get_text("output_image"), interactive=False, elem_classes=["output-image"])
+        output_image = gr.Image(label=lang_util.get_text("output_image"), interactive=False, type="filepath", elem_classes=["output-image"])
+        output_image.change(self._set_output_image, inputs=[output_image])
         clipboard_button = gr.Button(""+lang_util.get_text("clipboard"), elem_classes=["clipboard"])
-        clipboard_button.click(self._notify, inputs=[self.output_image], _js=javascript)
+        clipboard_button.click(self._notify, inputs=[output_image], _js=javascript, queue=True)
         if self.transfer_target is not None:
-            transfer_button = gr.Button(lang_util.get_text(self.transfer_target.lang_key))
-            transfer_button.click(self.transfer_target.accept_transfer, inputs=[self.output_image])
-        return self.output_image
+            transfer_button = gr.Button(lang_util.get_text(self.transfer_target.transfer_lang_key))
+            transfer_button.click(lambda :self.transfer_target.accept_transfer(self.output_image_path))
+        return output_image
 
-    def _notify(self, output_image):
-        if output_image is None:
+    def _set_output_image(self, output_image_path):
+        self.output_image_path = output_image_path
+
+    def _notify(self):
+        if self.output_image_path is None:
             gr.Warning("Please Image Select")
         else:
             gr.Info("Image Copied to Clipboard")
