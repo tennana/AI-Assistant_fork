4c943c940c86652918b6bcb4dda25b3626e0bd3c
Tennana <tennana@tennana.net>
2024-06-09 01:41:26 +0900

    feat: タブ間転送(img2img => 線画化)

--- AI_Assistant_modules/output_image_gui.py
+++ AI_Assistant_modules/output_image_gui.py
@@ -16,19 +16,23 @@ function copyToClipboard() {
 }
 """
 
+
 class OutputImage:
-    def __init__(self, transfer_target=None):
+    def __init__(self, transfer_target_lang_key=None):
+        self.transfer_button = None
+        self.output_image = None
         self.output_image_path = None
-        self.transfer_target = transfer_target
+        self.transfer_target_lang_key = transfer_target_lang_key
 
     def layout(self, lang_util):
-        output_image = gr.Image(label=lang_util.get_text("output_image"), interactive=False, type="filepath", elem_classes=["output-image"])
+        output_image = gr.Image(label=lang_util.get_text("output_image"), interactive=False, type="filepath",
+                                elem_classes=["output-image"])
         output_image.change(self._set_output_image, inputs=[output_image])
-        clipboard_button = gr.Button(""+lang_util.get_text("clipboard"), elem_classes=["clipboard"])
+        clipboard_button = gr.Button("" + lang_util.get_text("clipboard"), elem_classes=["clipboard"])
         clipboard_button.click(self._notify, inputs=[output_image], _js=javascript, queue=True)
-        if self.transfer_target is not None:
-            transfer_button = gr.Button(lang_util.get_text(self.transfer_target.transfer_lang_key))
-            transfer_button.click(lambda :self.transfer_target.accept_transfer(self.output_image_path))
+        if self.transfer_target_lang_key is not None:
+            self.transfer_button = gr.Button(lang_util.get_text(self.transfer_target_lang_key))
+        self.output_image = output_image
         return output_image
 
     def _set_output_image(self, output_image_path):
