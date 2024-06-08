590902494bf4914b98296262534cecd422c7f43e
Tennana <tennana@tennana.net>
2024-06-08 16:14:31 +0900

    fixup! feat: 出力画像UI

--- AI_Assistant_modules/output_image_gui.py
+++ AI_Assistant_modules/output_image_gui.py
@@ -11,3 +11,4 @@ class OutputImage:
         if self.transfer_target is not None:
             transfer_button = gr.Button(lang_util.get_text(self.transfer_target.lang_key))
             transfer_button.click(self.transfer_target.accept_transfer, inputs=[output_image])
+        return output_image
